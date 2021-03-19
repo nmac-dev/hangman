d2e160900f7a0db8b7fb87d9ab80694fd4be1f25
nmac-dev <ntwmac.dev@gmail.com>
2021-03-19 23:32:28 +0000

    Code cleanup

--- hangman/LoadResources.cs
+++ hangman/LoadResources.cs
@@ -14,12 +14,12 @@ namespace hangman {
 
         /*      Directories     */
         private static readonly string dirImages = "hangman.resources.images.";
-        private static readonly string dirWords = "hangman.resources.words.";
+        private static readonly string dirWords  = "hangman.resources.words.";
 
         /*      File names      */
-        private static string txtEasy = "easy";
+        private static string txtEasy   = "easy";
         private static string txtMedium = "medium";
-        private static string txtHard = "hard";
+        private static string txtHard   = "hard";
 
         /** Load a text file resource (filtered by difficulty) */
         internal static string[] loadTxtFile(int difficulty) {
@@ -27,6 +27,7 @@ namespace hangman {
             string[] arString = null;
             // Gets the file name (without extention) based on the selected difficulty
             string fileName = difficulty switch {
+
                 4 => dirWords + txtEasy,
                 6 => dirWords + txtMedium,
                 8 => dirWords + txtHard
@@ -36,13 +37,18 @@ namespace hangman {
                 using (Stream stream = assembly.GetManifestResourceStream($"{fileName}.txt")) {
                     try {
                         using (StreamReader sReader = new StreamReader(stream)) {
+
                             arString = sReader.ReadToEnd().Split('\n');
                         }
-                    } catch (FileFormatException e) {
+                    } 
+                    catch (FileFormatException e) {
+
                         MessageBox.Show($"Exception: {fileName}.txt is the wrong format... {e.Message}");
                     }
                 }
-            } catch (FileNotFoundException e) {
+            }
+            catch (FileNotFoundException e) {
+
                 MessageBox.Show($"Exception: {fileName}.txt was not found... {e.Message}");
             }
             return arString;
@@ -50,11 +56,13 @@ namespace hangman {
 
         /** Loads all .png files (total of 12) */
         internal static BitmapImage[] loadImages(int maxLives) {
+
             BitmapImage tempBmI;
             BitmapImage[] arImages = new BitmapImage[maxLives];
             int reverse = maxLives - 1;
 
             for(int i = 1; i < maxLives +1; i++, reverse--) {
+                
                 try {
                     using (Stream stream = assembly.GetManifestResourceStream($"{dirImages + i}.png")) {
 
@@ -62,11 +70,13 @@ namespace hangman {
 
                         tempBmI.BeginInit();
                         tempBmI.StreamSource = stream;
-                        tempBmI.CacheOption = BitmapCacheOption.OnLoad;
+                        tempBmI.CacheOption  = BitmapCacheOption.OnLoad;
                         tempBmI.EndInit();
-                        arImages[reverse] = tempBmI;
+                        arImages[reverse]    = tempBmI;
                     }
-                } catch (FileNotFoundException e) {
+                } 
+                catch (FileNotFoundException e) {
+
                     MessageBox.Show($"Exception: {dirImages + i}.png was not found... {e.Message}");
                 }
             }

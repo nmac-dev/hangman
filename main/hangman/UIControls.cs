ff3489deb791f2d303b3743aeb562adc4f39482e
nmac-dev <ntwmac.dev@gmail.com>
2021-04-06 20:48:08 +0100

    Code Correction

--- main/hangman/UIControls.cs
+++ main/hangman/UIControls.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace hangman {
